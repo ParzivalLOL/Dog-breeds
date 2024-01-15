3f3d9bf23dcb2d4fe717c5e0d48983091ed1d7f1
ParzivalLOL <86312982+ParzivalLOL@users.noreply.github.com>
2024-01-15 10:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def upload():
     # Save the uploaded file to the upload folder
 
     # Process the image file (you can add your own image processing logic here)
-    path_to_best_pt = "C:/Users/shukl/OneDrive/Desktop/Programming/AI/dogAI/best.pt"
+    path_to_best_pt = "best.pt"
     model = YOLO(path_to_best_pt)
     file = request.files['file']
     image = Image.open(file)
